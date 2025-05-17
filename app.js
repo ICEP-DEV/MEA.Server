b3709248ed90469f473cf7d8fcf8f4a9d306d9ca
kabelo-mastermind <0604749776kb@gmail.com>
2025-05-17 12:46:40 +0200

    changed port number

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ app.use(cors());  // This allows all origins by default
 // Ensure this middleware is added before any route handling
 app.use(express.json());
 
-app.use('/api', usageRoutes);  // Prefix with /api
+app.use('/api', usageRoutes);  // Prefix with /api 
 app.use('/api', sustainabilityRoutes);  // Prefix with /api
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
